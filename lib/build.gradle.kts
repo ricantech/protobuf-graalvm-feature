dc9b96fee2c39450c8d50c9b291738b1c802de77
ricantech <michal.rican@gmail.com>
2023-10-13 12:48:18 +0200

    Change version to alpha

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -32,7 +32,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/ricantech/REPOSITORY")
+            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
